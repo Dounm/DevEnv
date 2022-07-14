b6b8bb840239786e8d4f1ec3ddebd8ad545be7b6
niuchong <niuchong@inspirai.com>
2022-07-14 18:33:14 +0800

    feat(nvim): set vista width from 50->30

--- nvim/lua/vista_conf.lua
+++ nvim/lua/vista_conf.lua
@@ -2,7 +2,7 @@ vim.cmd
 [[
 let g:vista_icon_indent = ["╰─▸ ", "├─▸ "]
 let g:vista_default_executive = 'coc'
-let g:vista_sidebar_width=50
+let g:vista_sidebar_width=30
 let g:vista#renderer#enable_icon = 1
 let g:vista#renderer#icons = {
 \   "function": "\uf794",
